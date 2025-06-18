bff9da49d620c5b475eb3ed62c62dc323a99ea1c
CHIRAG TOMAR <147633073+029-CHIRAG@users.noreply.github.com>
2025-06-18 12:52:10 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay, paymentStripe, verifyStripe } from '../controllers/userController.js';
+import { loginUser, loginNormal,registerNormal,getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay, paymentStripe, verifyStripe } from '../controllers/userController.js';
 import upload from '../middleware/multer.js';
 import authUser from '../middleware/authUser.js';
 import { downloadReceipt } from '../controllers/userController.js';
 const userRouter = express.Router();
 
 userRouter.get("/login", loginUser)
+userRouter.post("/loginNormal",loginNormal);
+userRouter.post("/registerNormal",registerNormal);
 
 userRouter.get("/get-profile", authUser, getProfile)
 userRouter.post("/update-profile", upload.single('image'), authUser, updateProfile)
